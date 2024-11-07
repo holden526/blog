86e9afbdf85db25836caae919529b0aeb64a2a9a
rachel.li <rachel.li@ubtrobot.com>
2024-11-07 14:06:09 +0800

    feat:文章更新、字数、阅读时间信息

--- docs/.vitepress/plugins/ReadingTimePlugin.ts
+++ docs/.vitepress/plugins/ReadingTimePlugin.ts
@@ -1,5 +1,6 @@
-import type { Plugin } from 'vite'
+import { Plugin } from 'vite'
 import { getReadingTime } from '../utils/getReadingTime'
+import fs from 'fs'
 
 export function ReadingTimePlugin(): Plugin {
   return {
@@ -7,9 +8,16 @@ export function ReadingTimePlugin(): Plugin {
     enforce: 'pre',
     async transform(code, id) {
       if (!id.match(/\.md\b/)) return null
+
+      // 获取文件的最近更新时间
+      const lastUpdated = getLastUpdatedTime(id)
+
+      // 获取阅读时间和字数
       const { readTime, words } = getReadingTime(code)
+
+      // 插入阅读时间、字数和最近更新时间到文章中
       code = insertReadingTimeAndWords(
-        `<ReadingTime readTime="${readTime}" words="${words}" />`,
+        `<ReadingTime readTime="${readTime}" words="${words}" lastUpdated="${lastUpdated}" />`,
         code
       )
       return code
@@ -17,7 +25,14 @@ export function ReadingTimePlugin(): Plugin {
   }
 }
 
-// 插入阅读时间字数组件到第一个一级标题后
+// 获取文件的最近更新时间
+function getLastUpdatedTime(filePath: string): string {
+  const stats = fs.statSync(filePath)
+  const lastModifiedTime = stats.mtime
+  return lastModifiedTime.toLocaleString() // 格式化成字符串
+}
+
+// 插入阅读时间、字数和最近更新时间到第一个一级标题后
 function insertReadingTimeAndWords(target: string, source: string) {
   const headerRegex = /(^#\s.+$)/m
   return source.replace(headerRegex, `$1\n\n${target}`)
